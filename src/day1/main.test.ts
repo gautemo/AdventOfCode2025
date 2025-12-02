aae7e7c849fa0ea4773139eeb1b6e95174edc8f4
Gaute Meek Olsen <gaute.meek@gmail.com>
2025-12-02 22:05:45 +0100

    day1

--- src/day1/main.test.ts
+++ src/day1/main.test.ts
@@ -1,12 +1,21 @@
 import { assertEquals } from '@std/assert'
 import { part1, part2 } from './main.ts'
 
+const input = `L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`
+
 Deno.test('part1 example', () => {
-  const input = ``
-  assertEquals(part1(input), 0)
+  assertEquals(part1(input), 3)
 })
 
 Deno.test('part2 example', () => {
-  const input = ``
-  assertEquals(part2(input), 0)
+  assertEquals(part2(input), 6)
 })
--- src/day1/main.ts
+++ src/day1/main.ts
@@ -1,11 +1,42 @@
-// deno-lint-ignore no-unused-vars
 export function part1(input: string) {
-  return 0
+  let dial = 50
+  let sum = 0
+  for (const { dir, dist } of getRotations(input)) {
+    for (let i = 0; i < Number(dist); i++) {
+      dial = rotate(dial, dir)
+    }
+    if (dial === 0) {
+      sum++
+    }
+  }
+  return sum
 }
 
-// deno-lint-ignore no-unused-vars
 export function part2(input: string) {
-  return 0
+  let dial = 50
+  let sum = 0
+  for (const { dir, dist } of getRotations(input)) {
+    for (let i = 0; i < Number(dist); i++) {
+      dial = rotate(dial, dir)
+      if (dial === 0) {
+        sum++
+      }
+    }
+  }
+  return sum
+}
+
+function getRotations(input: string) {
+  return input.split('\n').map((line) => {
+    return line.match(/(?<dir>R|L)(?<dist>\d+)/)!.groups as { dir: 'L' | 'R'; dist: string }
+  })
+}
+
+function rotate(number: number, dir: 'L' | 'R', min = 0, max = 99) {
+  const next = number + (dir === 'L' ? -1 : 1)
+  if (next < min) return max
+  if (next > max) return min
+  return next
 }
 
 if (import.meta.main) {
